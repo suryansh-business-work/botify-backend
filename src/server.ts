82c24ec56af1d62ba519cb56743f4e5a0e551496
Suryansh Srivastava <suryansh.business.work@gmail.com>
2025-07-14 16:00:27 +0530

    Fix cors

--- src/server.ts
+++ src/server.ts
@@ -62,9 +62,14 @@ app.use('/api-docs/design-system', swaggerUi.serve, swaggerUi.setup(swaggerDocum
 // Middleware
 // ==========
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://botify.life'
+  ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD']
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
